23b78ba2cf3b5a7747fb79c6b4c656e3397a14f5
Jānis Basovs <janis_basis@hotmail.com>
2024-01-12 03:57:30 +0200

    Refactor navbar links

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,31 +3,19 @@ import Link from "next/link"
 export const Navbar = () => {
   return (
     <div className="flex gap-4 p-2">
-      <Link href="/" prefetch={false} className="py-2 px-4 bg-blue-400">
+      <Link href="/" className="py-2 px-4 bg-blue-400">
         Home
       </Link>
 
-      <Link
-        href="random-user-1"
-        prefetch={false}
-        className="py-2 px-4 bg-blue-400"
-      >
+      <Link href="random-user-1" className="py-2 px-4 bg-blue-400">
         RandomUser 1
       </Link>
 
-      <Link
-        href="random-user-2"
-        prefetch={false}
-        className="py-2 px-4 bg-blue-400"
-      >
+      <Link href="random-user-2" className="py-2 px-4 bg-blue-400">
         RandomUser 2
       </Link>
 
-      <Link
-        href="random-user-3"
-        prefetch={false}
-        className="py-2 px-4 bg-blue-400"
-      >
+      <Link href="random-user-3" className="py-2 px-4 bg-blue-400">
         RandomUser 3
       </Link>
     </div>
